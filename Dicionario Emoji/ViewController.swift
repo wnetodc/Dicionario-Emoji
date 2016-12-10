87c54e467488c0f47733cff66a7c331ac1e5a6a0
wellington neto <wellington.neto@gmail.com>
2016-12-10 17:45:36 -0200

    criação da mostraEmojiViewController

--- Dicionario Emoji/ViewController.swift
+++ Dicionario Emoji/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet var emojiTable: UITableView!
+    
+    var emojiList = ["😜","😍","🤑","😴","😎","🤘🏿"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiList.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell()
+        
+        print(indexPath.row)
+        cell.textLabel?.text = emojiList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "segueExibeEmoji", sender: emojiList[indexPath.row])
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print(sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojiTable.dataSource = self
+        emojiTable.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
